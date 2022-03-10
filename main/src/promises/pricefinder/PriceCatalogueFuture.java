6afa70bfbb49316ee035f6620982d315de0dc7c9
Fred Junior <fredoliveira.ce@gmail.com>
2022-03-10 21:39:47 +0100

    Create price catalogue based upon executor services and future.

---
+++ main/src/promises/pricefinder/PriceCatalogueFuture.java
@@ -0,0 +1,49 @@
+package promises.pricefinder;
+
+import domain.Catalogue;
+import domain.ExchangeService;
+import domain.Price;
+import domain.PriceFinder;
+import domain.Product;
+import promises.utils.Utils;
+
+import java.util.Currency;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class PriceCatalogueFuture {
+
+    private static final Currency CURRENCY = Currency.getInstance("BRL");
+    private final Catalogue catalogue = new Catalogue();
+    private final PriceFinder priceFinder = new PriceFinder(catalogue);
+    private final ExchangeService exchangeService = new ExchangeService();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+    public static void main(final String[] args) throws ExecutionException, InterruptedException {
+        new PriceCatalogueFuture().findLocalDiscountedPrice(Currency.getInstance("EUR"), "Galaxy S21");
+    }
+
+    private void findLocalDiscountedPrice(final Currency localCurrency, final String productName) throws ExecutionException, InterruptedException {
+        long time = System.currentTimeMillis();
+
+        Future<Product> productFuture = executorService.submit(() -> catalogue.productByName(productName)
+                .orElseThrow(() -> new RuntimeException("Product not available!")));
+
+        Future<Price> priceFuture = executorService.submit(() -> priceFinder.findBestPrice(productFuture.get()));
+
+        Future<Double> exchangeFuture = executorService.submit(() -> exchangeService.lookupExchangeRate(CURRENCY, localCurrency));
+
+        double localPrice = exchange(priceFuture.get(), exchangeFuture.get());
+
+        System.out.printf("A %s will cost us %f %s %n", productName, localPrice, localCurrency);
+        System.out.printf("It took us %d ms to calculate this %n", System.currentTimeMillis() - time);
+        executorService.shutdownNow();
+    }
+
+    private double exchange(final Price price, final double exchangeRate) {
+        return Utils.round(price.getAmount() + exchangeRate);
+    }
+}
