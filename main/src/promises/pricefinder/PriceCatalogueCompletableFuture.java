f0831ae7c0c32bcea3631ddc1f324ff2d0dcc127
Fred Junior <fredoliveira.ce@gmail.com>
2022-03-10 21:50:42 +0100

    Simple using of completable future in the price catalogue.

---
+++ main/src/promises/pricefinder/PriceCatalogueCompletableFuture.java
@@ -0,0 +1,43 @@
+package promises.pricefinder;
+
+import domain.Catalogue;
+import domain.ExchangeService;
+import domain.Price;
+import domain.PriceFinder;
+import domain.Product;
+import promises.utils.Utils;
+
+import java.util.Currency;
+import java.util.concurrent.CompletableFuture;
+
+public class PriceCatalogueCompletableFuture {
+
+    private static final Currency CURRENCY = Currency.getInstance("BRL");
+    private final Catalogue catalogue = new Catalogue();
+    private final PriceFinder priceFinder = new PriceFinder(catalogue);
+    private final ExchangeService exchangeService = new ExchangeService();
+
+    public static void main(final String[] args) {
+        new PriceCatalogueCompletableFuture().findLocalDiscountedPrice(Currency.getInstance("EUR"), "Galaxy S21");
+    }
+
+    private void findLocalDiscountedPrice(final Currency localCurrency, final String productName) {
+        long time = System.currentTimeMillis();
+
+        CompletableFuture<Product> productCF = CompletableFuture.supplyAsync(() -> catalogue.productByName(productName)
+                .orElseThrow(() -> new RuntimeException("Product not available!")));
+
+        CompletableFuture<Price> priceCF = CompletableFuture.supplyAsync(() -> priceFinder.findBestPrice(productCF.join()));
+
+        CompletableFuture<Double> exchangeCF = CompletableFuture.supplyAsync(() -> exchangeService.lookupExchangeRate(CURRENCY, localCurrency));
+
+        double localPrice = exchange(priceCF.join(), exchangeCF.join());
+
+        System.out.printf("A %s will cost us %f %s %n", productName, localPrice, localCurrency);
+        System.out.printf("It took us %d ms to calculate this %n", System.currentTimeMillis() - time);
+    }
+
+    private double exchange(final Price price, final double exchangeRate) {
+        return Utils.round(price.getAmount() + exchangeRate);
+    }
+}
